48b6cc9495e4a4e77020c63c00a07e2fbd3b39cb
Ajmal akram <96975779+Cyber-Aju@users.noreply.github.com>
2023-11-07 21:15:15 +0530

    Add files via upload

---
+++ Day 5/romanToInt.cpp
@@ -0,0 +1,43 @@
+/*
+https://leetcode.com/problems/roman-to-integer/description/
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char , int>m;
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(m[s[i]]<m[s[i+1]])
+            {
+                ans-=m[s[i]];
+            }
+            else
+            ans+=m[s[i]];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    string input;
+    cout << "Enter a Roman numeral: ";
+    cin >> input;
+    int result = solution.romanToInt(input);
+    cout << "The integer equivalent is: " << result << endl;
+    return 0;
+}
